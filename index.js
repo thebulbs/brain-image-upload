d76c427edc3d8d128ceaceea6b8b34f1729a7e5e
Matthias Laug <matthias.laug@gmail.com>
2017-06-15 15:25:40 +0200

    added s3 upload

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-
 var express = require('express')
 var cors = require('cors')
 var multer  = require('multer')
+var multerS3 = require('multer-s3')
+var AWS = require('aws-sdk')
+var s3 = new AWS.S3({signatureVersion: 'v4'})
 
-var storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, './uploads/')
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.originalname)
-  }
-})
-var upload = multer({ 
-	storage : storage,
-	dest: 'uploads/',
-})
 var app = express()
-app.use(express.static('uploads'));
 app.use(cors())
 
-app.post('/', upload.single('file'), function (req, res) {
-	return res.send('OK')
-});
+var upload = multer({
+    storage: multerS3({
+        s3: s3,
+        bucket: 'brain-mapper',
+        acl: 'public-read',
+        key: function (req, file, cb) {
+            cb(null, 'bulb/' + file.originalname);
+        }
+    })
+})
+
+app.post('/', upload.single('file'), function(req, res) {
+    res.send('Successfully uploaded')
+})
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+  console.log('Uploader listening on port 3000!')
 });
